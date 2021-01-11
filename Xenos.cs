a5fab7c9015c3f67c20a54517bfb1af81f810f28
Spectre <Spectre@DESKTOP-MUJD1UV>
2021-01-11 13:46:56 +0300

    Add project files.

---
+++ Xenos.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace killme
+{
+    public partial class Xenos : Form
+    {
+        public Xenos()
+        {
+            InitializeComponent();
+        }
+        Random rnd = new Random();
+        char[] letters = "ABCDEFGHIKLMNOPQRSTVXYZ0123456789".ToArray();
+        string output;
+
+        void generate(int NoOfLetter)
+        {
+            output = null;
+            for (int i=0; i< NoOfLetter; i++)
+            {
+
+                output += letters[rnd.Next(0, letters.Length)];
+            }
+            textBox1.Text = output;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            generate(20);
+        }
+    }
+}
